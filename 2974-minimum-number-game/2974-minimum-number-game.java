4dc268c5682be4b8d6c579944e9d7f7ded20467a
Udaya Giri <udayagiri7734@gmail.com>
2026-07-26 22:08:20 +0530

    Time: 5 ms (75.62%), Space: 47.4 MB (12.88%) - LeetHub

---
+++ 2974-minimum-number-game/2974-minimum-number-game.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int[] numberGame(int[] nums) {
+        Arrays.sort(nums);
+
+        int [] arr = new int [nums.length];
+
+        for (int i = 0 ; i < nums.length ; i += 2) {
+
+            arr [i] = nums [i + 1];
+            arr [i + 1] = nums [i];
+        }
+
+        return arr;
+    }
+}
