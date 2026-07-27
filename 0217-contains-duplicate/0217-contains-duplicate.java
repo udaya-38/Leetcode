98d87c24335cf5178435cbf8209b4b049aaa60be
Udaya Giri <udayagiri7734@gmail.com>
2026-07-27 13:48:17 +0530

    Time: 18 ms (57.48%), Space: 108.3 MB (16.26%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        Set <Integer> unique = new HashSet<>();
+
+        for (int num : nums) {
+
+            if (unique.contains(num)) {
+                return true;
+            }
+
+            unique.add(num);
+            
+        }
+
+        return false;
+    }
+}
